e6ce95bea4ffee878aecdb221cb6f5d37fb63a79
ngongoctam <ngongoctam103@gmail.com>
2021-10-01 21:53:00 +0700

    Session 1

---
+++ Session 1/index.js
@@ -0,0 +1,35 @@
+let message = 'Coding is great';
+console.log(message);
+
+let studentCount = 0;
+console.log(studentCount);
+
+message = 'Coding might not be easy, but still great';
+console.log(message);
+
+studentCount = 11;
+console.log(studentCount);
+
+message = message.toLowerCase();
+console.log(message);
+
+studentCount = studentCount + 1;
+console.log(studentCount);
+
+alert('You look beautifull today');
+
+let friend = prompt('Hello, what is your name?');
+alert(`Hi, ${friend}`);
+
+let ten = prompt('Tên của bạn là gì?');
+let ho = prompt('Họ của bạn là gì?');
+alert('Xin chào, ' + ho + ' ' + ten);
+
+let sVuong = prompt('Nhập chiều dài cạnh hình vuông');
+alert(`Diện tích hình vuông cạnh ${sVuong} là ${sVuong**2}`);
+
+let sTron = prompt('Nhập bán kính hình tròn');
+alert(`Diện tích hình tròn có bán kính ${sTron} là ${3.14*sTron**2}`);
+
+let doC = prompt('Nhập độ C');
+alert(`Độ F là ${doC*1.8+32}`);
