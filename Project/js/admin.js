081fb3db7992e782fb94183f562097d03314bfb5
ngongoctam <ngongoctam103@gmail.com>
2021-11-12 11:15:52 +0700

    update

---
+++ Project/js/admin.js
@@ -0,0 +1,94 @@
+var data = [
+       
+];
+
+
+function add() {
+
+
+
+    var xid = document.getElementById('id').value;
+    var xname = document.getElementById('name').value;
+    var xorigin = document.getElementById('origin').value
+    var xprice = document.getElementById('price').value
+
+
+    item = {
+        id: xid,
+        name: xname,
+       origin: xorigin,
+       price: xprice,
+    }
+
+
+  
+
+
+    let index = data.findIndex((c) => c.id === xid)
+    console.log(index)
+
+    if (index >= 0) {
+        data.splice(index, 1, item)
+    }
+    else {
+        data.push(item);
+    }
+    clear()
+    show();
+
+}
+
+
+
+
+function clear() {
+    document.getElementById('id').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('origin').value = '';
+    document.getElementById('price').value = '';
+}
+
+function show() {
+    var table = `<tr>
+                     <th>Id</th>
+                    <th>Name</th>
+                    <th>Origin</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>`
+
+    for (let i = 0; i < data.length; i++) {
+        table += `<tr>
+                     <td>${data[i].id}</td>
+                    <td>${data[i].name}</td>
+                    <td>${data[i].origin}</td>
+                    <td>${data[i].price}</td>
+                    <th>
+                        <button onclick="Delete(${data[i].id})">Delete</button>
+                        <button onclick="Edit(${data[i].id})">Edit</button>
+                        </th>
+                </tr>`
+    }
+    document.getElementById("bang").innerHTML = table
+}
+
+function Delete(id) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
+            data.splice(i, 1)
+            show();
+        }
+    }
+}
+function Edit(id) {
+    document.getElementById("ID");
+    for (let i = 0; i <= data.length; i++) {
+        if (data[i].id == id) {
+            document.getElementById('id').value = data[i].id;
+            document.getElementById('name').value = data[i].name;
+            document.getElementById('origin').value = data[i].origin;
+            document.getElementById('price').value = data[i].price;
+        }
+
+    }
+}
---
+++ Project/js/intro.js
@@ -0,0 +1,46 @@
+let girl = [
+    {
+        surname: 'Nguyễn Đức',
+        name: 'Tùng',
+        img: 'img/tung.jpg',
+        desc: 'Đăng nhập, Đăng kí'
+    },
+    {
+        surname: 'Nguyễn',
+        name: 'Quỳnh Anh',
+        img: 'img/qa.jpg',
+        desc:'Tìm kiếm api, Footer và Trang giới thiệu các thành viên'
+    },
+    {
+        surname: 'Đỗ Tuấn',
+        name: 'Dũng',
+        img: 'img/dung.jpg',
+        desc:'CRUD để tạo/xử lý thông tin sản phẩm'
+    },
+    {
+        surname: 'Ngô Ngọc',
+        name: 'Tâm',
+        img: 'img/tam.jpg',
+        desc:'Xử lý data, Xử lý tổng giao diện)'
+    },
+];
+let picture = document.querySelectorAll('.effect-sarah');
+picture.forEach((element, index) => {
+    element.lastElementChild.firstElementChild.prepend(girl[index].surname + ' ');
+    element.lastElementChild.firstElementChild.firstElementChild.innerText = girl[index].name;
+    element.firstElementChild.src = girl[index].img;
+    element.addEventListener('click', () => {
+        Swal.fire({
+            title: `<h2 class="name">${girl[index].surname} ${girl[index].name}<h2>`,
+            html: 
+            `<p class="desc">${girl[index].desc}</p>`
+            ,
+            
+            padding: '1em',
+            background: '#fff url(/images/trees.png)',
+            
+            
+        
+        })
+    })
+})
